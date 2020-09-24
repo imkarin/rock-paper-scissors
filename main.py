9760f1447316cacac02b71b103bf3e6be97b6319
karimeij <karinmeijvogel@hotmail.com>
2020-09-24 21:53:33 +0200

    :sparkles: Capturing video input with OpenCV!

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import cv2
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+cap = cv2.VideoCapture(0)
 
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('Video', frame)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+cap.release()
+cv2.destroyAllWindows()
